f7559b9e27fd1c69acebe4e581dcad25bdc2db6c
Dane <camaradane2000@hotmail.fr>
2025-12-09 18:37:26 -0500

    second commit

--- emotion_detection.py
+++ emotion_detection.py
@@ -2,19 +2,35 @@
 # Importer la bibliothèque requests pour gérer les requêtes HTTP
 import requests
 
-# Définir une fonction nommée emotion_detector qui prend une chaîne en entrée (text_to_analyse)
+# Importer la bibliothèque json
+import json
+
+
 def emotion_detector(text_to_analyse):
     """ 
-       Fonction qui prend un texte en argument pour detecter les émotions 
+       Ce code reçoit une chaine en entrée. Utilise l'url de service
+       pour détecter les émotions et définit les en têtes pour les requêtes API.
+       Crée un dictionnaire avec le texte à analyser.
+       Envoie une requête POST à l'API avec le texte et les en têtes
+       en fin retourne le texte de la réponse de l'API
     """
-    # URL du service de détection d'émotion
+
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    # Définir les en-têtes requis pour la requête API
+
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    # Créer un dictionnaire avec le texte à analyser
+
     MyJson = { "raw_document": { "text": text_to_analyse } }
-    # Envoyer une requête POST à l'API avec le texte et les en-têtes
+
     response = requests.post(url, json = MyJson, headers=header)
-    # Retourner le texte de la réponse de l'API
-    return response.text
+    formatted_response = json.loads(response.text)
+    emotion = formatted_response['emotionPredictions'][0]['emotion']
+    dominant_emotion = ""
+    score_dominant_emotion = 0
+    for emot in emotion:
+        score = emotion[emot]
+        if score > score_dominant_emotion:
+            score_dominant_emotion = score
+            dominant_emotion = emot
+    
+    return {'émotions': emotion,'dominant_emotion': dominant_emotion}
 
